9a8f5f6a36a6736a37c364d08e4ac747cca08785
Chintala-Keerthana <keerthiramana.44@gmail.com>
2026-07-08 22:06:29 +0530

    Fixed MySQL connection using connection pool

--- server/config/db.js
+++ server/config/db.js
@@ -1,23 +1,17 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
   ssl: {
     rejectUnauthorized: true,
   },
-});
-
-db.connect((err) => {
-  if (err) {
-    console.log("❌ Database Connection Failed!");
-    console.log(err);
-  } else {
-    console.log("✅ MySQL Connected Successfully!");
-  }
 });
 
 module.exports = db;
